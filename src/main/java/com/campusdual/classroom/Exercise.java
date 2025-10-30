6515f613353fdb6f2583418287c0429f71bdd1a6
Sofía Tello <tellosofia@outlook.com>
2025-10-30 20:47:29 +0100

    Exercise 23: Solved

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -1,18 +1,28 @@
 package com.campusdual.classroom;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Exercise {
     public static Queue<String> createQueue() {
+        Queue<String> myQueue = new LinkedList<>();
 
+        myQueue.offer("Smith");
+        myQueue.offer("Montessori");
+        myQueue.offer( "Peralta");
+        myQueue.offer("House");
+
+        return myQueue;
     }
     public static void printAndEmptyQueue(Queue<String> queue) {
-
+        for (int i = 0; i <= queue.size() + 2; i++) {
+            System.out.println(queue.peek());
+            queue.poll();
+        }
     }
 
     public static void main(String[] args) {
-
+        printAndEmptyQueue(createQueue());
     }
-
 }
